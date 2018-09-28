206743772e8cc67dac3821d8b4de7e5a3fe6e14c
Pranav M <m.pranav@ymail.com>
2018-09-28 22:19:28 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,14 +1,3 @@
- DEBUG=False
- ALLOWED_HOSTS=[*]
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+DEBUG=False
+ALLOWED_HOSTS=[*]
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
